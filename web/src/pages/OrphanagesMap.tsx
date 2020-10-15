cd2a6c734bb82e9d477d4b2799756daf8f31b30d
Roberto Nicoletti <roberto.urias@gmail.com>
2020-10-14 22:25:50 -0300

    NLW-Happy: Criando página de orfanato

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import mapMarkerImg from '../images/logo-mark.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
     return (
-        <h1>Orfanatos</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>São Paulo</strong>
+                    <span>Taboão da Serra</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#ffffff"/>
+            </Link>
+        </div>
     );
 }
 

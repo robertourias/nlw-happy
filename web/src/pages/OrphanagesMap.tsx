9b5de36009a0f7ee0ce280deb40c0c843953f277
Roberto Nicoletti <roberto.urias@gmail.com>
2020-10-14 22:47:24 -0300

    NLW-Happy: Criando mapa

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { Map, TileLayer } from 'react-leaflet';
 
 import mapMarkerImg from '../images/logo-mark.svg';
 
 import '../styles/pages/orphanages-map.css';
+import 'leaflet/dist/leaflet.css';
 
 function OrphanagesMap() {
     return (
@@ -23,7 +25,14 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <div></div>
+            <Map
+                center={[-23.6204244,-46.7978972]}
+                zoom={14}
+                style={{width: '100%', height: '100%'}}
+            >
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+            </Map>
 
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#ffffff"/>

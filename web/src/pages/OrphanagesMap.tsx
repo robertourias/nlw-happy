e2ac2ce6d9b1040c942442762e358dcce2255721
Roberto Nicoletti <roberto.urias@gmail.com>
2020-10-16 22:11:02 -0300

    NLW-Happy: Abstraindo componentes

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,19 +2,11 @@ import React from 'react';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import Leaflet from 'leaflet';
 
 import mapMarkerImg from '../images/logo-mark.svg';
+import mapIcon from '../utils/mapIcon';
 
 import '../styles/pages/orphanages-map.css';
-import 'leaflet/dist/leaflet.css';
-
-const mapIcon = Leaflet.icon({
-    iconUrl: mapMarkerImg,
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [172, 2],
-});
 
 function OrphanagesMap() {
     return (

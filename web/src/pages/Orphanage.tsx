028a2a2ec3de4924c3b9ef0211249c778985d671
Roberto Nicoletti <roberto.urias@gmail.com>
2020-10-16 22:00:52 -0300

    NLW-Happy: Criando rotas novas e componentizando Sidebar

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { FiClock, FiInfo, FiArrowLeft } from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
-import { useHistory } from 'react-router-dom';
 import L from 'leaflet';
+import Sidebar from '../components/Sidebar';
 
 import mapMarkerImg from '../images/logo-mark.svg';
 
@@ -18,19 +18,9 @@ const happyMapIcon = L.icon({
 })
 
 export default function Orphanage() {
-  const { goBack } = useHistory();
-
   return (
     <div id="page-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      <Sidebar />
 
       <main>
         <div className="orphanage-details">

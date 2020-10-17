2a44368374de140965dfda47953fa11de89fc412
Roberto Nicoletti <roberto.urias@gmail.com>
2020-10-16 21:13:19 -0300

    NLW-Happy: Add cors ao projeto

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import errorHandler from './errors/handler';
 import 'express-async-errors';
 import './database/connection';
 import routes from './routes';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));

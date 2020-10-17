d94a6df19cf3affb8b6d60b0e09cf1b8371707f9
Roberto Nicoletti <roberto.urias@gmail.com>
2020-10-16 21:05:24 -0300

    NLW-Happy: Criando error handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import path from 'path';
+import errorHandler from './errors/handler';
+import 'express-async-errors';
 import './database/connection';
 import routes from './routes';
 
@@ -8,4 +10,5 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 app.listen(3333);

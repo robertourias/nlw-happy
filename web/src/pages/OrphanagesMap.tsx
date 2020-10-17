3a31ea8a1696ea9d405b6d8eb8ebdeeab87d2ad2
Roberto Nicoletti <roberto.urias@gmail.com>
2020-10-16 21:39:59 -0300

    NLW-Happy: Crindo pontos no mapa

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import mapMarkerImg from '../images/logo-mark.svg';
 
 import '../styles/pages/orphanages-map.css';
 import 'leaflet/dist/leaflet.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [172, 2],
+});
 
 function OrphanagesMap() {
     return (
@@ -32,13 +40,25 @@ function OrphanagesMap() {
             >
                 {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
                 <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+                <Marker 
+                    icon={mapIcon}
+                    position={[-23.6204244,-46.7978972]}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar das meninas
+                        <Link to="">
+                            <FiArrowRight size={20} color="#fff" />
+                        </Link>
+                    </Popup>
+                </Marker>
             </Map>
 
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#ffffff"/>
             </Link>
         </div>
-    );
+    );  
 }
 
 export default OrphanagesMap;
